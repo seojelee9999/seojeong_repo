c7d7d8b64882a7ed6d7494301e98509045b9ac46
seojelee9999 <lsj2sj03@gmail.com>
2023-12-24 09:40:20 +0900

    merge_myself

--- sorting/merge_sort.c
+++ sorting/merge_sort.c
@@ -1,36 +1,15 @@
 #include <stdio.h>
+
 #define SIZE 10
-int sorted[SIZE];
 
-void merge(int list[], int left, int mid, int right){
-  int i, j, k, l;
-  i = left;
-  j = mid+1;
-  k = left;
-
-  /* 분할 정렬된 list의 합병 */
-  while(i<=mid && j<=right){
-    if(list[i]<=list[j])
-      sorted[k++] = list[i++];
-    else
-      sorted[k++] = list[j++];
-  }
-
-  // 남아 있는 값들을 일괄 복사
-  if(i>mid){
-    for(l=j; l<=right; l++)
-      sorted[k++] = list[l];
-  }
-  // 남아 있는 값들을 일괄 복사
-  else{
-    for(l=i; l<=mid; l++)
-      sorted[k++] = list[l];
-  }
-
-  // 배열 sorted[](임시 배열)의 리스트를 배열 list[]로 재복사
-  for(l=left; l<=right; l++){
-    list[l] = sorted[l];
-  }
+void merge_sort(int list[], int left, int right);
+void merge(int list[], int left, int mid, int right);
+int main()
+{
+	int arr[] = {1,3,5,7,9,2,4,6,8,0};
+	merge_sort(arr,0,9);
+	for(int i = 0 ; i < SIZE; i++)
+		printf("%d ",arr[i]);
 }
 void merge_sort(int list[], int left, int right)
 {
@@ -43,10 +22,33 @@ void merge_sort(int list[], int left, int right)
 		merge(list, left, mid, right);
 	}
 }
-int main()
+void merge(int list[], int left, int mid, int right)
 {
-	int arr[] = {1,3,5,7,9,2,4,6,8,0};
-	merge_sort(arr,0,9);
-	for (int i = 0 ; i < SIZE; i++)
-		printf("%d ",arr[i]);	
+	int i;
+	int	j;
+	int k;
+	int tmp[SIZE];
+
+	i = left;
+	j = mid + 1;
+	k = left;
+	while (i <= mid && j <= right)
+	{
+		if (list[i] > list[j])
+			tmp[k++] = list[j++];
+		else
+			tmp[k++] = list[i++];			
+	}
+	if (i <= mid)
+	{
+		while (i <= mid)
+			tmp[k++] = list[i++];
+	}
+	else
+	{
+		while (j <= right)
+			tmp[k++] = list[j++];
+	}
+	for (i = left; i <= right ; i++)
+		list[i] = tmp[i];
 }
