2c67eacca9a0438491994de697d8056e2a90defd
seojelee9999 <lsj2sj03@gmail.com>
2023-12-26 09:52:43 +0900

    demand cnt_sort

--- sorting/counting_sort.c
+++ sorting/counting_sort.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-#define MAX 10000
-void	counting_sort(int arr[], int n);
+
+void	count_sort(int list[], int n, int max_num)
+{
+	int cnt[max_num + 1];
+	int tmp[n];
+	for (int i = 0 ; i< max_num + 1; i++)
+		cnt[i] = 0;
+	for (int i = 0; i < n; i++)
+		cnt[list[i]]++;
+	for (int i = 0; i < max_num; i++)
+		cnt[i + 1] += cnt[i];
+	for (int i = n - 1; i >= 0; i--)
+	{
+		tmp[cnt[list[i]] - 1] = list[i];
+		cnt[list[i]]--;
+	}
+	for (int i = 0 ; i< n; i++)
+		list[i] = tmp[i];
+}
 
 int main()
 {
 	int arr[] = {1,3,5,7,9,2,4,6,8,0};
-	counting_sort(arr,10);
-	for (int i = 0 ; i< 10 ; i++)
+	count_sort(arr,10,9);
+	for (int i = 0; i < 10; i++)
 		printf("%d ",arr[i]);
-}
-void	counting_sort(int arr[], int n)
-{
-	int cnt[MAX + 1]; //cnt 배열의 크기는 배열의 최댓값+1 : 배열의 값이 cnt배열의 인덱스 번호로 들어가기 때문이다.
-	int tmp[MAX + 1];
-	for (int i = 0; i < MAX + 1; i++)
-		cnt[i] = 0;
-	for (int i = 0; i < n; i++)
-		cnt[arr[i]]++;
-	for (int i = 0; i < MAX; i++)
-		cnt[i + 1] += cnt[i];
-	for (int i = n - 1; i >= 0; i--)
-	{
-		tmp[cnt[arr[i]] - 1] = arr[i];
-		cnt[arr[i]]--;
-	}
-	for (int i = 0 ; i< n ;i ++)
-		arr[i] = tmp[i];
 }
