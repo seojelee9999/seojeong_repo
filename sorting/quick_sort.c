9efd0d2af03de84570787977e06fbc13d34e36cb
seojelee9999 <lsj2sj03@gmail.com>
2023-12-28 15:21:43 +0900

    quick_sort.c

--- sorting/quick_sort.c
+++ sorting/quick_sort.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void	quick_sort(int list[], int start, int end);
+void q_sort(int arr[], int start, int end) {
+    if (start >= end) return; // Base case
 
-int	main()
-{
-	int list[] = {1,3,5,7,9,2,4,6,8,0};
-	quick_sort(list,0,9);
-	for (int i = 0 ; i< 10 ;i++)
-		printf("%d ",list[i]);
+    int pivot = arr[start]; // Choose the first element as the pivot
+    int low = start + 1;
+    int high = end;
+
+    while (low <= high) {
+        while (low <= end && arr[low] < pivot) low++;
+        while (high > start && arr[high] > pivot) high--;
+
+        if (low < high) {
+            // Swap arr[low] and arr[high]
+            int tmp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = tmp;
+        } else {
+            break;
+        }
+    }
+
+    // Swap arr[start] (pivot) with arr[high]
+    int tmp = arr[start];
+    arr[start] = arr[high];
+    arr[high] = tmp;
+
+    // Recursively sort the two partitions
+    q_sort(arr, start, high - 1);
+    q_sort(arr, high + 1, end);
 }
-void	quick_sort(int list[], int start, int end)
-{
-	int	pibot;
-	int	tmp;
-	int	high;
-	int	low;
 
-	pibot = start;
-	low = pibot + 1;
-	high = end;
-	if (start < end)
-	{
-		while (low <= high)
-		{
-			while (list[low] < list[pibot])
-				low++;
-			while (list[high] > list[pibot])
-				high--;
-			if (low > high)
-			{
-				tmp = list[pibot];
-				list[pibot] = list[high];
-				list[high] = tmp;
-			}
-			else
-			{
-				tmp = list[high];
-				list[high] = list[low];
-				list[low] = tmp;
-			}
-		}
-		quick_sort(list, start, high -1);
-		quick_sort(list, high + 1, end);
-	}
+int main()
+{
+	int arr[] = {1,3,5,7,9,2,4,6,8,0,42};
+	q_sort(arr, 0, 10);
+	for (int i = 0; i < 11; i++)
+		printf("%d ", arr[i]);
 }
